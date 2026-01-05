8b209e81b8dfd2ff64b09c3924f547a695445f56
Duy Tu <duytu89@gmail.com>
2026-01-05 22:18:05 +0700

    dev

--- Vudaco/Debits/Repositories/DebitRepositories.cs
+++ Vudaco/Debits/Repositories/DebitRepositories.cs
@@ -1449,7 +1449,7 @@ namespace Vudaco.Debits.Repositories
                         GROUP BY receipt_id
                 ) d ON d.receipt_id = r.id
                 WHERE 
-                        (r.status IS NULL OR r.status = 1)
+                        (r.status IS NULL OR r.status = 0)
                         AND iecat.type = 1
                         AND iecat.parent_id in (12)
                         AND r.deleted_at IS NULL";
